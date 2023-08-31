09f964ceb2dc7a7370e6e7959f6f074cf950f330
bena <115331768+teshan-nuwantha@users.noreply.github.com>
2023-09-01 00:14:59 +0530

    registation form backend completed

--- Request Form/Request.php
+++ Request Form/Request.php
@@ -11,6 +11,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}else {
+    echo "<script>console.log('connected' );</script>";
 }
 
 // Retrieve data from the form
