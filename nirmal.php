f5f7f334282aac425586303f33432a1a78cdc05b
Diluk Mihiranga <dilukedu@gmail.com>
2023-09-05 18:49:57 +0530

    dynamic form

--- nirmal.php
+++ nirmal.php
@@ -1,24 +1,5 @@
-<?php
-// Step 3: Database Connection
-$conn = mysqli_connect("localhost", "root", "", "CINEC_SSSQ");
-$userID = $_GET['id'];
 
-// Step 4: Fetch Data
-$sql = "SELECT * FROM request_form,lecturer_names WHERE request_form.request_id = $userID";
-$result = mysqli_query($conn, $sql);
-
-// Step 5: Generate the Form
-echo '<form>';
-while ($row = mysqli_fetch_assoc($result)) {
-    echo '<input type="text" name="Course_name" value="' . $row['program_name'] . '"><br>';
-    echo '<input type="text" name="Course_Code" value="' . $row['program_code'] . '"><br>';
-    echo '<input type="text" name="batch" value="' . $row['batch_no'] . '"><br>';
-    echo '<input type="date" name="date" value="'. date('Y-m-d').'"><br>';
-    // Add more input fields for other user data
-    
-}
-
-echo '<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -34,31 +15,52 @@ echo '<!DOCTYPE html>
 </head>
 <body>
 <div class="container mt-5">
-    <h2 class="text-left">
+    <center><h2 class="text-left">
         STUDENT SATISFACTION SURVEY QUESTIONNAIRE
-    </h2>
+    </h2></center>
     <p class="text-left">
         Dear Course Delegate/Student,<br>
         Measurement of student satisfaction is of vital importance to evaluate organizations performance. This questionnaire may help us to identify deficiencies, strengths, weaknesses, threats, risks, and opportunities required to be recognized. Your perception of our service will lead to incremental improvements and others could be significantly beneficial for all interested parties. However, the implementation of improvements remains entirely at the discretion of the management, as proper planning, allocation of funds, and the impact on the organization need to be evaluated before action. Students necessarily may not always be right. Complaints in particular need to be investigated by the relevant head of Departments/Sections for validity and accuracy. Management intends to fulfill the needs and expectations of the student to an extent affordable to the management. The implementation of corrections and corrective action is the responsibility of the Head of Department/section.
     </p>
-    <form action="/submit" method="post">
-        <div class="form-group">
+<?php
+// Step 3: Database Connection
+$conn = mysqli_connect("localhost", "root", "", "CINEC_SSSQ");
+$userID = $_GET['id'];
+
+// Step 4: Fetch Data
+$sql = "SELECT * FROM request_form WHERE request_id = $userID";
+$result = mysqli_query($conn, $sql);
+
+$sql2 = "SELECT * FROM lecturer_names WHERE request_id = $userID";
+$result2 = mysqli_query($conn, $sql2);
+
+// Step 5: Generate the Form
+echo '<form>';
+while ($row = mysqli_fetch_assoc($result)) {
+    echo '<input type="text" name="Course_name" value="' . $row['program_name'] . '"><br>';
+   
+echo '<form action="/submit" method="post">';
+echo'       <div class="form-group">
             <label for="Course">Course:</label>
-            <input type="text" class="form-control" id="CourseName" placeholder="Enter Your Course Name" name="name" required>
-        </div>
-        <div class="form-group">
+            <input type="text" class="form-control" id="CourseName" placeholder="Enter Your Course Name" name="name" value="'.$row['program_name'].'" required>
+        </div>';
+
+echo' <div class="form-group">
             <label for="CourseCode">Course Code:</label>
-            <input type="text" class="form-control" id="CourseCode" placeholder="Enter Course Code" name="CourseCode" required>
-        </div>
-        <div class="form-group">
+            <input type="text" class="form-control" id="CourseCode" placeholder="Enter Course Code" name="CourseCode" value="'.$row['program_code'].'" required>
+        </div>';
+
+echo' <div class="form-group">
             <label for="Batch">Batch:</label>
-            <input type="text" class="form-control" id="Batch" placeholder="Enter Your Batch" name="Batch" required>
-        </div>
-        <div class="form-group">
+            <input type="text" class="form-control" id="Batch" placeholder="Enter Your Batch" name="Batch" value="'.$row['batch_no'].'" required>
+        </div>';
+
+echo' <div class="form-group">
             <label for="calendar">Date Of Survey:</label>
-            <input type="date" class="form-control" id="calendar" placeholder="Enter Date Of Survey" name="calendar" required>
-        </div>
-        <div class="form-group">
+            <input type="date" class="form-control" id="calendar" placeholder="Enter Date Of Survey" name="calendar" value="'. date('Y-m-d').'" required>
+        </div>';
+
+echo'<div class="form-group">
             <label for="Name1">Mr. Dhanushka Agalakada:</label>
             <table class="table">
                 <thead>
@@ -88,7 +90,17 @@ echo '<!DOCTYPE html>
                 </tbody>
             </table>
         </div>
-        <div class="form-group">
+       ';
+}
+
+
+// echo '</form>';
+
+// Step 8: Close Database Connection
+mysqli_close($conn);
+?>
+
+<div class="form-group">
             <p>
                 <label for="TTLP">Teaching Training Learning Process:</label><br>
                 Please Comment on Standard of Course Material, Lecture Notes, Presentation & Teaching Method, Attendance, Attitudes & Behaviors.
@@ -177,9 +189,3 @@ echo '<!DOCTYPE html>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
-';
-// echo '</form>';
-
-// Step 8: Close Database Connection
-mysqli_close($conn);
-?>
