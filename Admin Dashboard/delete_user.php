409aa5462ef87a0e1892966ffd2001a135fa6820
Diluk Mihiranga <dilukedu@gmail.com>
2023-09-18 03:49:54 +0530

    delete user completed. but server side foreign key error

---
+++ Admin Dashboard/delete_user.php
@@ -0,0 +1,25 @@
+<?php
+include('../db_connection.php');
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user_id = $_POST["user_id"];
+    
+    // Delete the user with the given ID from the database
+    $sql = "DELETE FROM users WHERE user_id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $user_id);
+    
+    if ($stmt->execute()) {
+        echo "User deleted successfully.";
+    } else {
+        echo "Error deleting user: " . $conn->error;
+    }
+    
+    $stmt->close();
+}
+
+// Close the database connection
+$conn->close();
+?>
---
+++ Admin Dashboard/users.php
@@ -0,0 +1,28 @@
+<?php
+include('../db_connection.php');
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch user data from the database
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $users = array();
+    while ($row = $result->fetch_assoc()) {
+        $users[] = $row;
+    }
+} else {
+    $users = array();
+}
+
+// Close the database connection
+$conn->close();
+
+// Return the user data as JSON
+header('Content-Type: application/json');
+echo json_encode($users);
+?>
