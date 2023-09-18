c198d5b7ed7e77b5e16773608850774c856722a6
Dananjaya_Rajarathna <83546013+123ddr@users.noreply.github.com>
2023-09-18 08:13:48 +0530

    lec_and_course_forms

---
+++ Dashboard1/courses.php
@@ -0,0 +1,32 @@
+<?php
+include('../db_connection.php');
+
+// Check if the connection was successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else {
+    echo "<script>console.log('connected' );</script>";
+}
+
+// Assuming you have form handling logic to retrieve data from the POST request
+$courseCode = $_POST['course_code'];
+$courseName = $_POST['course_name'];
+$departmentID = $_POST['dep_id'];
+
+// SQL query to insert data into the database
+$sql = "INSERT INTO course (course_code, course_name, dep_id)
+        VALUES ('$courseCode', '$courseName', '$departmentID')";
+
+
+
+// Execute the query
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+
+?>
---
+++ Dashboard1/lecturers.php
@@ -0,0 +1,32 @@
+<?php
+include('../db_connection.php');
+
+// Check if the connection was successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else {
+    echo "<script>console.log('connected' );</script>";
+}
+
+// Assuming you have form handling logic to retrieve data from the POST request
+$lecturerID = $_POST['lec_id'];
+$lecturerName = $_POST['lec_name'];
+$departmentID = $_POST['dep_id'];
+
+// SQL query to insert data into the database
+$sql = "INSERT INTO lecturer (lec_id, lec_name, dep_id)
+        VALUES ('$lecturerID', '$lecturerName', '$departmentID')";
+
+
+
+// Execute the query
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+
+?>
